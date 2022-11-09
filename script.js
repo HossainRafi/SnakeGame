265c9685131cb304e8927b172b6a581e5c4a9314
HossainRafi <hossainrafi71@gmail.com>
2022-11-09 12:24:54 +0600

    collide function done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Variables
+// All Variables
 let inputDir = { x: 0, y: 0 };
 const foodSound = new Audio("music/food.mp3");
 const gameOverSound = new Audio("music/gameover.mp3");
@@ -10,12 +10,33 @@ let lastPaintTime = 0;
 let snakeArr = [{ x: 13, y: 15 }];
 food = { x: 6, y: 7 };
 
-// Game Functions
+// Game Function
 function main(ctime) {
-    window.requestAnimationFrame(main);
-    if((ctime - lastPaintTime)/1000 < 1/speed){
-        return;
+  window.requestAnimationFrame(main);
+  if ((ctime - lastPaintTime) / 1000 < 1 / speed) {
+    return;
+  }
+  lastPaintTime = ctime;
+  gameEngine();
+}
+
+// Collide Function
+function isCollide(snake) {
+  // collide with self
+  for (let i = 1; i < snakeArr.length; i++) {
+    if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+      return true;
     }
-    lastPaintTime = ctime;
-    gameEngine();
+  }
+  // collide with the wall
+  if (
+    snake[0].x >= 18 ||
+    snake[0].x <= 0 ||
+    snake[0].y >= 18 ||
+    snake[0].y <= 0
+  ) {
+    return true;
+  }
+
+  return false;
 }
